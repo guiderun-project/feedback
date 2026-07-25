3663df2b507abace1d34d5b87caf628b361e4786
Pridesd <whwotjr98@naver.com>
2026-07-25 23:05:17 +0900

    feat: add feedback form submit logic with tests

---
+++ app.test.js
@@ -0,0 +1,7 @@
+const assert = require('assert');
+const { isValidFeedback } = require('./app.js');
+
+assert.strictEqual(isValidFeedback('좋은 프로그램이었어요'), true);
+assert.strictEqual(isValidFeedback('   '), false);
+assert.strictEqual(isValidFeedback(''), false);
+console.log('app.test.js: all tests passed');
